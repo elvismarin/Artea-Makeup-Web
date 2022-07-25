53bd865427809890a6eff7caa84e244558407bb6
Elvis Marin <elvismarin95@gmail.com>
2022-07-24 21:48:45 -0300

    Array

--- artea.js
+++ artea.js
@@ -1,5 +1,16 @@
 
-//let nombre = prompt ("Por favor ingresa los nombres de tus amigos.") ;
+
+const productos = ["Mascarillas","Paletas", "Brochas", "Sombras", "Labiales","Delineadores"] ;
+
+let enTotal = console.log (productos.length);
+
+
+
+
+
+
+
+////let nombre = prompt ("Por favor ingresa los nombres de tus amigos.") ;
 //let amigos = "" ;
 
 //while ( nombre != "salir" ) {
@@ -16,32 +27,32 @@
 
 
 
-let comprador = prompt ("Bienvenido a Artea ¿usted es comprador? Responda con SI o NO:") ;
+//let comprador = prompt ("Bienvenido a Artea ¿usted es comprador? Responda con SI o NO:") ;
 
-while ( comprador != "SI" && comprador != "NO") { 
+//while ( comprador != "SI" && comprador != "NO") { 
 
-  alert ("Por favor ingrese una respuesta valida.");
-  comprador = prompt ("Bienvenido a Artea ¿usted es comprador? Responda con SI o NO:");
+  //alert ("Por favor ingrese una respuesta valida.");
+  //comprador = prompt ("Bienvenido a Artea ¿usted es comprador? Responda con SI o NO:");
 
-} ;
+//} ;
 
-if (comprador == "SI" ) {
-  CantProd() ;
-} 
+//if (comprador == "SI" ) {
+  //CantProd() ;
+//} 
 
-else if ( comprador == "NO" ) {
-  alert ("¡Hasta la proxima!") ; } ;
+//else if ( comprador == "NO" ) {
+  //alert ("¡Hasta la proxima!") ; } ;
 
 
-function CantProd () {
+//function CantProd () {
 
-  let prod1 = parseInt (prompt ("¿Cuantas pestañas quiere?")) ;
-  let prod2 = parseInt (prompt ("¿Cuantas mascarillas quiere?")) ;
-  let prod3 = parseInt (prompt ("¿Cuantas paletas quiere?")) ;
+  //let prod1 = parseInt (prompt ("¿Cuantas pestañas quiere?")) ;
+  //let prod2 = parseInt (prompt ("¿Cuantas mascarillas quiere?")) ;
+  //let prod3 = parseInt (prompt ("¿Cuantas paletas quiere?")) ;
 
-  let prodtotal = prod1 + prod2 + prod3 ;
+  //let prodtotal = prod1 + prod2 + prod3 ;
 
-  alert ("En total usted lleva " + prodtotal + " productos")
+  //alert ("En total usted lleva " + prodtotal + " productos")
 
-} ;
+//} ;
 
