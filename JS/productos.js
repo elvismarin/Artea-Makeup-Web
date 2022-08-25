d12c765ce44dcf890e91fd201512eae19d8e0f3e
Elvis Marin <elvismarin95@gmail.com>
2022-08-25 08:23:47 -0300

    Fetch

--- JS/productos.js
+++ JS/productos.js
@@ -10,40 +10,42 @@ class producto {
     }
 }
 
-const producto1 = new producto ("1","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/1.png", 1500)
-const producto2 = new producto ("2","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/2.png", 1600)
-const producto3 = new producto ("3","Otro producto", "../Multimedia/PRODUCTOS/3.png", 2000)
-const producto4 = new producto ("4","Otro otro producto", "../Multimedia/PRODUCTOS/4.png", 1800)
-const producto5 = new producto ("5","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/5.png", 1500)
-const producto6 = new producto ("6","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/6.png", 1600)
-const producto7 = new producto ("7","Otro producto", "../Multimedia/PRODUCTOS/7.png", 2000)
-const producto8 = new producto ("8","Otro otro producto", "../Multimedia/PRODUCTOS/8.png", 1800)
-const producto9 = new producto ("9","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/9.png", 1500)
-const producto10 = new producto ("10","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/10.png", 1600)
-const producto11 = new producto ("11","Otro producto", "../Multimedia/PRODUCTOS/11.png", 2000)
-const producto12 = new producto ("12","Otro otro producto", "../Multimedia/PRODUCTOS/12.png", 1800)
-const producto13 = new producto ("13","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/13.png", 1500)
-const producto14 = new producto ("14","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/14.png", 1600)
-const producto15 = new producto ("15","Otro producto", "../Multimedia/PRODUCTOS/15.png", 2000)
-const producto16 = new producto ("16","Otro otro producto", "../Multimedia/PRODUCTOS/16.png", 1800)
-const producto17 = new producto ("17","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/17.png", 1500)
-const producto18 = new producto ("18","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/18.png", 1600)
-const producto19 = new producto ("19","Otro producto", "../Multimedia/PRODUCTOS/19.png", 2000)
-const producto20 = new producto ("20","Otro otro producto", "../Multimedia/PRODUCTOS/20.png", 1800)
-const producto21 = new producto ("21","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/21.png", 1500)
-const producto22 = new producto ("22","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/22.png", 1600)
-const producto23 = new producto ("23","Otro producto", "../Multimedia/PRODUCTOS/23.png", 2000)
-const producto24 = new producto ("24","Otro otro producto", "../Multimedia/PRODUCTOS/24.png", 1800)
-const producto25 = new producto ("25","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/25.png", 1500)
-const producto26 = new producto ("26","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/26.png", 1600)
-const producto27 = new producto ("27","Otro producto", "../Multimedia/PRODUCTOS/27.png", 2000)
-const producto28 = new producto ("28","Otro otro producto", "../Multimedia/PRODUCTOS/28.png", 1800)
-const producto29 = new producto ("29","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/29.png", 1500)
-const producto30 = new producto ("30","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/30.png", 1600)
-const producto31 = new producto ("31","Otro producto", "../Multimedia/PRODUCTOS/31.png", 2000)
-const producto32 = new producto ("31","Otro otro producto", "../Multimedia/PRODUCTOS/32.png", 1800)
+const producto1 = new producto ("1","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/1.png", 12)
+const producto2 = new producto ("2","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/2.png", 10)
+const producto3 = new producto ("3","Otro producto", "../Multimedia/PRODUCTOS/3.png", 6)
+const producto4 = new producto ("4","Otro otro producto", "../Multimedia/PRODUCTOS/4.png", 40)
+const producto5 = new producto ("5","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/5.png", 12)
+const producto6 = new producto ("6","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/6.png", 10)
+const producto7 = new producto ("7","Otro producto", "../Multimedia/PRODUCTOS/7.png", 6)
+const producto8 = new producto ("8","Otro otro producto", "../Multimedia/PRODUCTOS/8.png", 40)
+const producto9 = new producto ("9","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/9.png", 12)
+const producto10 = new producto ("10","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/10.png", 10)
+const producto11 = new producto ("11","Otro producto", "../Multimedia/PRODUCTOS/11.png", 6)
+const producto12 = new producto ("12","Otro otro producto", "../Multimedia/PRODUCTOS/12.png", 40)
+const producto13 = new producto ("13","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/13.png", 12)
+const producto14 = new producto ("14","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/14.png", 10)
+const producto15 = new producto ("15","Otro producto", "../Multimedia/PRODUCTOS/15.png", 6)
+const producto16 = new producto ("16","Otro otro producto", "../Multimedia/PRODUCTOS/16.png", 40)
+const producto17 = new producto ("17","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/17.png", 12)
+const producto18 = new producto ("18","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/18.png", 10)
+const producto19 = new producto ("19","Otro producto", "../Multimedia/PRODUCTOS/19.png", 6)
+const producto20 = new producto ("20","Otro otro producto", "../Multimedia/PRODUCTOS/20.png", 40)
+const producto21 = new producto ("21","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/21.png", 12)
+const producto22 = new producto ("22","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/22.png", 10)
+const producto23 = new producto ("23","Otro producto", "../Multimedia/PRODUCTOS/23.png", 6)
+const producto24 = new producto ("24","Otro otro producto", "../Multimedia/PRODUCTOS/24.png", 40)
+const producto25 = new producto ("25","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/25.png", 12)
+const producto26 = new producto ("26","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/26.png", 10)
+const producto27 = new producto ("27","Otro producto", "../Multimedia/PRODUCTOS/27.png", 6)
+const producto28 = new producto ("28","Otro otro producto", "../Multimedia/PRODUCTOS/28.png", 40)
+const producto29 = new producto ("29","Eyes on me - Liquid eyeliner", "../Multimedia/PRODUCTOS/29.png", 12)
+const producto30 = new producto ("30","Lapiz de cejas retractil", "../Multimedia/PRODUCTOS/30.png", 10)
+const producto31 = new producto ("31","Otro producto", "../Multimedia/PRODUCTOS/31.png", 6)
+const producto32 = new producto ("31","Otro otro producto", "../Multimedia/PRODUCTOS/32.png", 40)
 
-const productos = []
+
+
+const productos=[];
 
 productos.push(producto1);
 productos.push(producto2);
@@ -78,6 +80,7 @@ productos.push(producto30);
 productos.push(producto31);
 productos.push(producto32);
 
+productosars = productos;
 
 const contenedorCarrito = document.getElementById("carrito-contenedor")
 
@@ -92,6 +95,7 @@ botonVaciar.addEventListener("click", () => {
 
 
 let carrito = []
+
 
 // Mostrar los productos 
 
@@ -119,8 +123,10 @@ function mostrarProductos(productos) {
     divProducto.appendChild(botonComprar);
 
     contenedorDeProductos.appendChild(divProducto);
-        
+    
     });
+
+    
 }
 
 // Agregar cosas al carrito
@@ -170,7 +176,7 @@ const actualizarCarrito = () => {
     }); 
 
     carrito.forEach(element => {
-        pre = pre + (element.precio * element.cantidad)  
+        pre = pre + (element.precio * element.cantidad * pesosARG)  
     });
 
 
@@ -207,6 +213,17 @@ document.addEventListener("DOMContentLoaded", () => {
         actualizarCarrito()
     }
 })
+
+
+/**PASAR PRECIO DE DOLAR A PESOS */
+
+fetch("https://open.er-api.com/v6/latest/USD")
+.then( res => res.json())
+.then( data => {
+    pesosARG = (data.rates.ARS)});
+
+
+
 
 
 
